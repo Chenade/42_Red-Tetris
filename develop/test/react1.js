54e967c3cf1ba3da412a717136723de723497e3d
Chenade <chenade0312@gmail.com>
2024-05-12 16:17:26 +0200

    fixed test utils

--- develop/test/react1.js
+++ develop/test/react1.js
@@ -1,8 +1,8 @@
 import chai from "chai"
 import React from 'react'
 import equalJSX from 'chai-equal-jsx'
-import {createRenderer} from 'react-addons-test-utils'
-import {Tetris, Board} from '../src/client/components/test'
+import {createRenderer} from 'react-test-renderer/shallow'
+import {Game} from '../src/client/components/Game';
 
 chai.should()
 chai.use(equalJSX)
@@ -10,7 +10,7 @@ chai.use(equalJSX)
 describe('Fake react test', function(){
   it('works', function(){
     const renderer = createRenderer()
-    renderer.render(React.createElement(Tetris))
+    renderer.render(React.createElement(Game))
     const output = renderer.getRenderOutput()
     output.should.equalJSX(<Board/>)
   })
